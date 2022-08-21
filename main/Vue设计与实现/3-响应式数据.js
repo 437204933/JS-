7e7db0650c69b6f955b250e52f5883883b50e17c
437204933 <72692118+437204933@users.noreply.github.com>
2022-08-21 15:10:22 +0800

    feat(响应式数据): watch 函数

--- main/Vue设计与实现/3-响应式数据.js
+++ main/Vue设计与实现/3-响应式数据.js
@@ -17,16 +17,19 @@ var obj = new Proxy(data, {
 var activeEffectStack = [];
 var activeEffect;
 // 应付迭代嵌套
-function effect (fn) {
+function effect (fn, option = {}) {
     const effectFn = () => {
         clearEffect(fn)
         activeEffect = effectFn
         activeEffectStack.push(fn)
-        fn()
+        const res = fn()
         activeEffectStack.pop(fn)
         activeEffect = activeEffectStack[activeEffectStack.length - 1];
+        return res;
     }
+    effectFn.option = option
     effectFn.deps = []
+    if (!option.lazy) effectFn()
     return effectFn;
 }
 
@@ -67,8 +70,15 @@ function trigger (target, key) {
     if (!depsMap) return
     const effects = depsMap.get(key)
     const effectToRun = new Set(effects)
-    effectToRun && effectToRun.forEach(effectFn => 
-        effectFn !== activeEffect && effect()
+    effectToRun && effectToRun.forEach(effectFn => {
+        if (effectFn !== activeEffect) {
+            if (effectFn.option.scheduler) {
+                effectFn.option.scheduler(effectFn)
+            } else {
+                effectFn()
+            }
+        }
+    }
     )
 }
 
@@ -78,3 +88,81 @@ args.forEach(() => {
     arg.add(1)
     console.log('do');
 })
+
+// 定义一个任务队列
+var jobQueue = new Set()
+var p = Promise.resolve()
+
+let isFishing = false
+function flushJob() {
+    if (!isFishing) return
+    isFishing = true
+    p.then(() => jobQueue.forEach(fn => fn())).finally(() => {
+        isFishing = false
+    })
+}
+
+function computed(getter) {
+    let value
+    let dirty = true
+    // 获取值
+    const effectFn = effect(getter, {
+        lazy: true, 
+        scheduler: () => {
+            dirty = true
+            trigger(obj, 'value')
+        }
+    })
+    const obj = {
+        get value () {
+            if (dirty) {
+                value = effectFn()
+                dirty = false
+            }
+            return value
+        }
+    }
+    return obj
+}
+
+function watch (source, cb, options = {}) {
+    let getter;
+    if (typeof source === 'function') {
+        getter = source
+    } else {
+        getter = () => traverse(source)
+    }
+    let oldValue, newValue;
+
+    function job () {
+        newValue = effectFn()
+        cb(oldValue, newValue)
+        oldValue = newValue
+    }
+
+    const effectFn = effect(() => getter(), {
+        lazy: true, // 设置 lazy 属性手动调用获取旧值，再次调用时获取最新值
+        scheduler: () => {
+            if (option.flush === 'post') {
+                const p = Promise.resolve();
+                p,then(job)
+            } else {
+                job()
+            }
+        }
+    })
+
+    if (option.immediate) {
+        job()
+    }
+}
+
+function traverse (value, seen = new Set()) {
+    if (typeof value !== 'object' || value === null || seen.has(value)) return
+    seen.add(value)
+    // 对所有属性都进行遍及，当任何属性变化都活触发函数
+    for (const k in value) {
+        traverse(value[k], seen)
+    }
+    return value
+}
